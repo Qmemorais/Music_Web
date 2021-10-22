606c0b476865b882bea20163aaff5c5f8c85d87a
Qmemorais <qmemorais@gmail.com>
2021-10-23 00:44:52 +0300

    FinalServices

--- DataL/Context/MusicContext.cs
+++ DataL/Context/MusicContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataLayer.Context
 {
@@ -19,7 +20,29 @@ namespace DataLayer.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // использование Fluent API
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new PlaylistConfiguration());
+            modelBuilder.ApplyConfiguration(new SongConfiguration());
+        }
+        public class UserConfiguration : IEntityTypeConfiguration<User>
+        {
+            public void Configure(EntityTypeBuilder<User> builder)
+            {
+                builder.HasAlternateKey(u => u.Email);
+            }
+        }
+        public class PlaylistConfiguration : IEntityTypeConfiguration<Playlist>
+        {
+            public void Configure(EntityTypeBuilder<Playlist> builder)
+            {
+                builder.Property(b => b.Name).IsRequired();
+            }
+        }
+        public class SongConfiguration : IEntityTypeConfiguration<Song>
+        {
+            public void Configure(EntityTypeBuilder<Song> builder)
+            {
+            }
         }
     }
 }
