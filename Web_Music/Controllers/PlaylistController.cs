7455b9f3f56ab1df74e1c5003ab0a578351fbdd8
Qmemorais <qmemorais@gmail.com>
2021-11-13 22:16:27 +0200

    Update Controllers 2
    Update methods in PlaylistController/PlaylistSongController. Add new method from Service like AddSongToPlaylist

--- Web_Music/Controllers/PlaylistController.cs
+++ Web_Music/Controllers/PlaylistController.cs
@@ -4,22 +4,23 @@ using BusinessLayer.Services.Interface;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Net;
 using Web_Music.Models;
 
 namespace Web_Music.Controllers
 {
-    [Route("[controller]")]
+    [Route("[controller]s")]
     public class PlaylistController: Controller
     {
         private readonly IPlaylistService _playlistService;
-        private readonly IMapper _mappedPlaylists;
+        private readonly IMapper _mapper;
 
 
-        public PlaylistController(IPlaylistService playlistService, IMapper mappedPlaylists)
+        public PlaylistController(IPlaylistService playlistService, IMapper mapper)
         {
             _playlistService = playlistService;
-            _mappedPlaylists = mappedPlaylists;
+            _mapper = mapper;
         }
 
         [HttpGet("{playlistId}")]
@@ -31,8 +32,29 @@ namespace Web_Music.Controllers
                 var playlist = _playlistService.GetPlaylist(playlistId);
                 if (playlist == null)
                     return NotFound();
-                var getPlaylist = _mappedPlaylists.Map<PlaylistResponseModel>(playlist);
-                return Ok(getPlaylist);
+
+                var mappedPlaylist = _mapper.Map<PlaylistResponseModel>(playlist);
+                return Ok(mappedPlaylist);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<PlaylistResponseModel>), StatusCodes.Status200OK)]
+        public IActionResult GetAllUsers()
+        {
+            try
+            {
+                var playlists = _playlistService.GetAllPlaylists();
+                if (playlists == null)
+                    return NotFound();
+
+                var mappedPlaylists = _mapper.Map<IEnumerable<UserResponseModel>>(playlists);
+
+                return Ok(mappedPlaylists);
             }
             catch (Exception ex)
             {
@@ -48,8 +70,8 @@ namespace Web_Music.Controllers
                 if (requestModel == null)
                     return BadRequest();
 
-                var playlist = _mappedPlaylists.Map<PlaylistCreateDto>(requestModel);
-                _playlistService.CreatePlaylist(playlist);
+                var mappedPlaylist = _mapper.Map<PlaylistCreateDto>(requestModel);
+                _playlistService.CreatePlaylist(mappedPlaylist);
 
                 return StatusCode((int)HttpStatusCode.Created);
             }
@@ -67,6 +89,7 @@ namespace Web_Music.Controllers
                 var playlist = _playlistService.GetPlaylist(playlistId);
                 if (playlist == null)
                     return NotFound();
+
                 _playlistService.DeletePlaylist(playlistId);
                 return NoContent();
             }
@@ -81,8 +104,8 @@ namespace Web_Music.Controllers
         {
             try
             {
-                var playlistToUpdate = _mappedPlaylists.Map<PlaylistUpdateDto>(requestModel);
-                _playlistService.UpdatePlaylist(playlistId, playlistToUpdate);
+                var mappedPlaylistToUpdate = _mapper.Map<PlaylistUpdateDto>(requestModel);
+                _playlistService.UpdatePlaylist(playlistId, mappedPlaylistToUpdate);
                 return NoContent();
             }
             catch (Exception ex)
--- Web_Music/Controllers/PlaylistSongController.cs
+++ Web_Music/Controllers/PlaylistSongController.cs
@@ -4,22 +4,23 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using Web_Music.Models;
 
 namespace Web_Music.Controllers
 {
-    [Route("[controller]")]
+    [Route("[Playlist/{playlistId}/songs]")]
     public class PlaylistSongController : Controller
     {
         private readonly ISongService _songService;
         private readonly IPlaylistService _playlistService;
-        private readonly IMapper _mappedSong;
+        private readonly IMapper _mapper;
 
-        public PlaylistSongController(ISongService songService, IPlaylistService playlistService, IMapper mappedSong)
+        public PlaylistSongController(ISongService songService, IPlaylistService playlistService, IMapper mapper)
         {
             _songService = songService;
             _playlistService = playlistService;
-            _mappedSong = mappedSong;
+            _mapper = mapper;
         }
 
         [HttpGet("{playlistId}")]
@@ -31,11 +32,40 @@ namespace Web_Music.Controllers
                 var getPlaylist = _playlistService.GetPlaylist(playlistId);
                 if (getPlaylist == null)
                     return NotFound();
+
                 var allSongsByPlaylist = _songService.GetAllSongsByPlaylist(playlistId);
                 if (allSongsByPlaylist == null)
                     return NotFound();
-                var getAllSongsByPlaylist = _mappedSong.Map<IEnumerable<SongResponseModel>>(allSongsByPlaylist);
+
+                var getAllSongsByPlaylist = _mapper.Map<IEnumerable<SongResponseModel>>(allSongsByPlaylist);
                 return Ok(getAllSongsByPlaylist);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+
+        //[HttpGet("{songId}")]
+
+        [HttpPost]
+        public IActionResult AddPlaylistToUser(int playlistId, int songId)
+        {
+            try
+            {
+                var song = _songService.GetSongById(songId);
+
+                if (song == null)
+                    return NotFound();
+
+                var playlist = _playlistService.GetPlaylist(playlistId);
+
+                if (playlist == null)
+                    return NotFound();
+
+                _playlistService.AddSongToPlaylist(playlistId,songId);
+
+                return StatusCode((int)HttpStatusCode.Created);
             }
             catch (Exception ex)
             {
