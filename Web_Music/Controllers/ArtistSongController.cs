4b7d1e48cbce1537cf413d31311a62866ef80d3a
Qmemorais <qmemorais@gmail.com>
2021-11-14 23:23:33 +0200

    Create ArtistSongController.cs
    Add controler to get list of songs which exist in artist model

---
+++ Web_Music/Controllers/ArtistSongController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Web_Music.Models;
+using AutoMapper;
+using BusinessLayer.Services.Interface;
+using Microsoft.AspNetCore.Http;
+
+namespace Web_Music.Controllers
+{
+    [Route("[Artist/{artistId}/songs]")]
+    public class ArtistSongController : ControllerBase
+    {
+        private readonly ISongService _songService;
+        private readonly IMapper _mapper;
+
+
+        public ArtistSongController(ISongService songService, IMapper mapper)
+        {
+            _songService = songService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{artistId}")]
+        [ProducesResponseType(typeof(IEnumerable<SongResponseModel>), StatusCodes.Status200OK)]
+        public IActionResult GetAllSongssByArtist([FromRoute] int artistId)
+        {
+            try
+            {
+                var songs = _songService.GetAllSongsByArtist(artistId);
+                if (songs == null)
+                    return NotFound();
+
+                var mappedSongs = _mapper.Map<IEnumerable<SongResponseModel>>(songs);
+
+                return Ok(mappedSongs);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+    }
+}
