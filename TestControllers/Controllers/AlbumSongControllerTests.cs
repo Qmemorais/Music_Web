73fa9c99515a42bd97bc3b7f1eb84a0d90bd1b08
Qmemorais <qmemorais@gmail.com>
2021-12-04 18:01:11 +0200

    Update AlbumSongControllerTests.cs
    create tests for method to get all songs by album in albumSongController

--- TestControllers/Controllers/AlbumSongControllerTests.cs
+++ TestControllers/Controllers/AlbumSongControllerTests.cs
@@ -1,20 +1,106 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Web_Music.Controllers;
-using System;
-using System.Collections.Generic;
+using AutoFixture;
+using Moq;
+using AutoMapper;
+using BusinessLayer.Services.Interface;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BusinessLayer.Models;
+using Web_Music.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Web_Music.Controllers.Tests
 {
     [TestClass()]
     public class AlbumSongControllerTests
     {
-        [TestMethod()]
-        public void GetAllSongsByAlbumTest()
+        private Mock<ISongService> mockSongService;
+        private Mock<IAlbumService> mockAlbumService;
+        private Mock<IMapper> mapper;
+        private Fixture fixture;
+
+        private AlbumSongController controller;
+
+        private readonly int have = 1, no = 0;
+
+        [TestInitialize]
+        public void Initialize()
         {
-            Assert.Fail();
+            fixture = new Fixture();
+            fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            mockAlbumService = new Mock<IAlbumService>();
+            mockSongService = new Mock<ISongService>();
+            mapper = new Mock<IMapper>();
+
+            controller = new AlbumSongController(mockAlbumService.Object, mockSongService.Object, mapper.Object);
+        }
+
+        [TestMethod()]
+        public void GetAllSongsByAlbumTest_WithExistAlbumAndSongs_ReturnList()
+        {
+            var songs = fixture.CreateMany<SongDto>();
+            var songsResponse = songs.Select(songDto => fixture.Build<SongResponseModel>()
+                .With(x => x.AlbumId, songDto.AlbumId)
+                .With(x => x.ArtistId, songDto.ArtistId)
+                .With(x => x.Name, songDto.Name)
+                .With(x => x.Time, songDto.Time)
+                .Create());
+            var album = fixture.Create<AlbumDto>();
+
+            mapper.Setup(m => m.Map<IEnumerable<SongResponseModel>>(songs)).Returns(songsResponse);
+
+            mockAlbumService.Setup(service => service.GetAlbum(have)).Returns(album);
+            mockSongService.Setup(service => service.GetAllSongsByAlbum(have)).Returns(songs);
+            //act
+            var result = controller.GetAllSongsByAlbum(have) as OkObjectResult;
+            var responseModel = result?.Value;
+            //assert
+            Assert.IsInstanceOfType(responseModel, typeof(IEnumerable<SongResponseModel>));
+            Assert.AreEqual(songsResponse, responseModel);
+        }
+
+        [TestMethod()]
+        public void GetAllSongsByAlbumTest_WithUnexistAlbum_ReturnNotFound()
+        {
+            var songs = fixture.CreateMany<SongDto>();
+            var songsResponse = songs.Select(songDto => fixture.Build<SongResponseModel>()
+                .With(x => x.AlbumId, songDto.AlbumId)
+                .With(x => x.ArtistId, songDto.ArtistId)
+                .With(x => x.Name, songDto.Name)
+                .With(x => x.Time, songDto.Time)
+                .Create());
+            mapper.Setup(m => m.Map<IEnumerable<SongResponseModel>>(songs)).Returns(songsResponse);
+
+            mockAlbumService.Setup(service => service.GetAlbum(no)).Returns((AlbumDto)null);
+            mockSongService.Setup(service => service.GetAllSongsByAlbum(no)).Returns(songs);
+            //act
+            var result = controller.GetAllSongsByAlbum(no);
+            //assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+        [TestMethod()]
+        public void GetAllSongsByAlbumTest_WithUnexistSongs_ReturnNotFound()
+        {
+            var songs = fixture.CreateMany<SongDto>();
+            var songsResponse = songs.Select(songDto => fixture.Build<SongResponseModel>()
+                .With(x => x.AlbumId, songDto.AlbumId)
+                .With(x => x.ArtistId, songDto.ArtistId)
+                .With(x => x.Name, songDto.Name)
+                .With(x => x.Time, songDto.Time)
+                .Create());
+            var album = fixture.Create<AlbumDto>();
+
+            mapper.Setup(m => m.Map<IEnumerable<SongResponseModel>>(songs)).Returns(songsResponse);
+
+            mockAlbumService.Setup(service => service.GetAlbum(have)).Returns(album);
+            mockSongService.Setup(service => service.GetAllSongsByAlbum(have)).Returns((IEnumerable<SongDto>)null);
+            //act
+            var result = controller.GetAllSongsByAlbum(have);
+            //assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
         }
     }
 }
