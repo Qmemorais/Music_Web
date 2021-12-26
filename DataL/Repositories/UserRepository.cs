d12507ced2c1b3a48d21ed9f872bc60fef2c9925
Qmemorais <qmemorais@gmail.com>
2021-12-26 17:46:35 +0200

    Create UserRepository.cs
    create UserRepository to work with UofW

---
+++ DataL/Repositories/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using DataLayer.Context;
+using DataLayer.Interfaces;
+using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataLayer.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private MusicContext db;
+
+        public UserRepository(MusicContext context)
+        {
+            this.db = context;
+        }
+        public void Create(User user)
+        {
+            db.Users.Add(user);
+        }
+
+        public void Update(User user)
+        {
+            db.Entry(user).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user != null)
+                db.Users.Remove(user);
+        }
+
+        public IEnumerable<User> Find(Func<User, bool> predicate)
+        {
+            return db.Users.Include(u => u.Playlists).Where(predicate).ToList();
+        }
+
+        public User Get(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return db.Users.Include(u => u.Playlists);
+        }
+    }
+}
