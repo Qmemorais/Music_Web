026160ed24f7eae7b951b9845cf985db878c6ee4
Qmemorais <qmemorais@gmail.com>
2021-12-26 18:08:22 +0200

    Create IUnitOfWork.cs
    create IUnitOfWork to create class UnitOfWork to work with models in DB

---
+++ DataL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using DataLayer.Models;
+using System;
+
+namespace DataLayer.Interfaces
+{
+    interface IUnitOfWork : IDisposable
+    {
+        IRepository<User> Users { get; }
+        IRepository<Playlist> Playlists { get; }
+        IRepository<Song> Songs { get; }
+        IRepository<Album> Albums { get; }
+        IRepository<Artist> Artists { get; }
+        void Save();
+    }
+}
