73e95843bff3a4efe6f06d530e43bd9e695362b3
Qmemorais <qmemorais@gmail.com>
2021-12-26 17:51:15 +0200

    Create PlaylistRepository.cs
    create PlaylistRepository to work with UofW

---
+++ DataL/Repositories/PlaylistRepository.cs
@@ -0,0 +1,52 @@
+﻿using DataLayer.Context;
+using DataLayer.Interfaces;
+using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataLayer.Repositories
+{
+    class PlaylistRepository : IRepository<Playlist>
+    {
+        private MusicContext db;
+
+        public PlaylistRepository(MusicContext context)
+        {
+            this.db = context;
+        }
+
+        public void Create(Playlist playlist)
+        {
+            db.Playlists.Add(playlist);
+        }
+        
+        public void Update(Playlist playlist)
+        {
+            db.Entry(playlist).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            Playlist playlist = db.Playlists.Find(id);
+            if (playlist != null)
+                db.Playlists.Remove(playlist);
+        }
+
+        public IEnumerable<Playlist> Find(Func<Playlist, bool> predicate)
+        {
+            return db.Playlists.Include(p => p.Users).Include(p => p.Songs).Where(predicate).ToList();
+        }
+
+        public Playlist Get(int id)
+        {
+            return db.Playlists.Find(id);
+        }
+
+        public IEnumerable<Playlist> GetAll()
+        {
+            return db.Playlists.Include(p => p.Users).Include(p => p.Songs);
+        }
+    }
+}
