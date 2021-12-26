7c5c147d0fbce2408b8040e5a70f25ea368541be
Qmemorais <qmemorais@gmail.com>
2021-12-26 18:32:51 +0200

    Create UnitOfWork.cs
    create unitOfWork to work with DB by repositories

---
+++ DataL/Repositories/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using DataLayer.Context;
+using DataLayer.Interfaces;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repositories
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        private readonly MusicContext _db;
+        private UserRepository User;
+        private SongRepository Song;
+        private PlaylistRepository Playlist;
+        private AlbumRepository Album;
+        private ArtistRepository Artist;
+
+        public UnitOfWork(MusicContext context)
+        {
+            _db = context;
+        }
+
+        public IRepository<User> Users => User ??= new UserRepository(_db);
+        public IRepository<Song> Songs => Song ??= new SongRepository(_db);
+        public IRepository<Playlist> Playlists => Playlist ??= new PlaylistRepository(_db);
+        public IRepository<Album> Albums => Album ??= new AlbumRepository(_db);
+        public IRepository<Artist> Artists => Artist ??= new ArtistRepository(_db);
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
