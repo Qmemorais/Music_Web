f8af69d94a667b623a42191af88cbd3b17a2470c
Qmemorais <qmemorais@gmail.com>
2021-12-26 16:36:33 +0200

    Update User.cs
    update field "Email" to unique. add new fileds "age" and "country" to User

--- DataL/Models/User.cs
+++ DataL/Models/User.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace DataLayer.Models
 {
+    [Index("Email", IsUnique = true)]
     public class User
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; }
         public string Surname { get; set; }
         [Required]
         public string Email { get; set; }
+        [Required]
+        public int Age { get; set; }
+        public string Country { get; set; }
         public virtual List<Playlist> Playlists { get; set; } = new List<Playlist>();
     }
 }
