7cc3177ecb2469283cb8e218233f78cbab6bdbcf
Qmemorais <qmemorais@gmail.com>
2021-12-27 16:42:20 +0200

    Update DB
    update ForeignKey to Guid type

--- DataL/Migrations/20211227143427_UpdateFields.Designer.cs
+++ DataL/Migrations/20211227143427_UpdateFields.Designer.cs
@@ -4,14 +4,16 @@ using DataLayer.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataLayer.Migrations
 {
     [DbContext(typeof(MusicContext))]
-    partial class MusicContextModelSnapshot : ModelSnapshot
+    [Migration("20211227143427_UpdateFields")]
+    partial class UpdateFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataL/Migrations/20211227143427_UpdateFields.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLayer.Migrations
+{
+    public partial class UpdateFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Songs_Albums_AlbumId1",
+                table: "Songs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Songs_Artists_ArtistId1",
+                table: "Songs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Songs_AlbumId1",
+                table: "Songs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Songs_ArtistId1",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "AlbumId1",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "ArtistId1",
+                table: "Songs");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ArtistId",
+                table: "Songs",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AlbumId",
+                table: "Songs",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AtristId",
+                table: "Albums",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_AlbumId",
+                table: "Songs",
+                column: "AlbumId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_ArtistId",
+                table: "Songs",
+                column: "ArtistId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Songs_Albums_AlbumId",
+                table: "Songs",
+                column: "AlbumId",
+                principalTable: "Albums",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Songs_Artists_ArtistId",
+                table: "Songs",
+                column: "ArtistId",
+                principalTable: "Artists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Songs_Albums_AlbumId",
+                table: "Songs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Songs_Artists_ArtistId",
+                table: "Songs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Songs_AlbumId",
+                table: "Songs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Songs_ArtistId",
+                table: "Songs");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ArtistId",
+                table: "Songs",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AlbumId",
+                table: "Songs",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AlbumId1",
+                table: "Songs",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ArtistId1",
+                table: "Songs",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AtristId",
+                table: "Albums",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_AlbumId1",
+                table: "Songs",
+                column: "AlbumId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_ArtistId1",
+                table: "Songs",
+                column: "ArtistId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Songs_Albums_AlbumId1",
+                table: "Songs",
+                column: "AlbumId1",
+                principalTable: "Albums",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Songs_Artists_ArtistId1",
+                table: "Songs",
+                column: "ArtistId1",
+                principalTable: "Artists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
