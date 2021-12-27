1a7f47c6b5c6a0edda385ccc682ff069cbff3dc7
Qmemorais <qmemorais@gmail.com>
2021-12-27 16:09:03 +0200

    Create SongDTOToGet.cs
    create song model to get all info from DB

---
+++ BusinessLayer/Models/SongDTOToGet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Models
+{
+    public class SongDTOToGet
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public DateTime Time { get; set; }
+        public Guid AuthorId { get; set; }
+        public Guid AlbumId { get; set; }
+        public List<PlaylistDTO> Playlists { get; set; }
+    }
+}
