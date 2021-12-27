3c4effbe8d5ccc3f4982b3c631c55e94a13fe237
Qmemorais <qmemorais@gmail.com>
2021-12-27 16:27:52 +0200

    Create AlbumCreateDTO.cs
    create model to add new Album to DB

---
+++ BusinessLayer/Models/AlbumCreateDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BusinessLayer.Models
+{
+    public class AlbumCreateDTO
+    {
+        public string Name { get; set; }
+        public Guid AtristId { get; set; }
+    }
+}
