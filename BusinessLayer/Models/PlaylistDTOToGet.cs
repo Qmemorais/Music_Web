1527e268dd34830df5de6bde803e49fb76796914
Qmemorais <qmemorais@gmail.com>
2021-12-27 15:55:22 +0200

    Create PlaylistDTOToGet.cs
    create model of Playlist to get all info from DB

---
+++ BusinessLayer/Models/PlaylistDTOToGet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Models
+{
+    public class PlaylistDTOToGet
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid UserOwnerId { get; set; }
+        public DateTime TimeToPlay { get; set; }
+        public List<UserDTO> Users { get; set; }
+        public List<SongDTO> Songs { get; set; }
+    }
+}
