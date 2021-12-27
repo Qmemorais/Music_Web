aa12c9f45a53b4efacf87dec97c8ac8c21d2e837
Qmemorais <qmemorais@gmail.com>
2021-12-27 16:24:37 +0200

    Create AlbumDTOToGet.cs
    create album model to get all info from DB

---
+++ BusinessLayer/Models/AlbumDTOToGet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Models
+{
+    public class AlbumDTOToGet
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public Guid AtristId { get; set; }
+        public List<SongDTO> Songs { get; set; }
+    }
+}
