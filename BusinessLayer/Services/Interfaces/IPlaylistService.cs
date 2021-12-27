0a5ac9bf34373aeafa50ccddda8495ee309f043e
Qmemorais <qmemorais@gmail.com>
2021-12-27 17:09:20 +0200

    Create IPlaylistService.cs
    create methods interfaces for Playlist

---
+++ BusinessLayer/Services/Interfaces/IPlaylistService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLayer.Models;
+
+namespace BusinessLayer.Services.Interfaces
+{
+    public interface IPlaylistService
+    {
+        public void CreatePlaylist(PlaylistCreateDTO playlistCreate);
+        public void UpdatePlaylist(PlaylistUpdateDTO playlistUpdate);
+        public void DeletePlaylist(Guid id);
+        public List<PlaylistDTOToGet> GetPlaylists();
+        public PlaylistDTOToGet GetPlaylistById(Guid id);
+        public void AddSongToPlaylist(Guid playlistId, Guid songId);
+        public List<PlaylistDTOToGet> GetAllPlaylistsByUser(Guid userId);
+        public List<PlaylistDTOToGet> GetAllPlaylistsBySong(Guid songId);
+        //take all playlists where time is more then we asked
+        public List<PlaylistDTOToGet> GetAllPlaylistsByTime(DateTime time);
+    }
+}
