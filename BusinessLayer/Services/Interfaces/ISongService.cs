08b3df562a00eef3839f9dda2ed377caa02e7b3d
Qmemorais <qmemorais@gmail.com>
2021-12-27 17:15:49 +0200

    Create ISongService.cs
    create methods interfaces for Song

---
+++ BusinessLayer/Services/Interfaces/ISongService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLayer.Models;
+
+namespace BusinessLayer.Services.Interfaces
+{
+    public interface ISongService
+    {
+        public void CreateSong(SongCreateDTO songCreate);
+        public void UpdateSong(SongUpdateDTO songUpdate, Guid id);
+        public void DeleteSong(Guid id);
+        public List<SongDTOToGet> GetSongs();
+        public SongDTOToGet GetSongById(Guid id);
+        public List<SongDTOToGet> GetAllSongsByPlaylist(Guid playlistId);
+        public List<SongDTOToGet> GetAllSongsByArtist(Guid artistId);
+        public List<SongDTOToGet> GetAllSongsByAlbum(Guid AlbumId);
+        //get all songs which have the same time to play
+        public List<SongDTOToGet> GetAllSongsByTime(DateTime time);
+    }
+}
