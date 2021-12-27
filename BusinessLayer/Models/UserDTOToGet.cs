0e144581b6da5c07b97ef8b28bf23e6f82378a81
Qmemorais <qmemorais@gmail.com>
2021-12-27 15:45:32 +0200

    Create UserDto
    create model UserDto take all info from user

---
+++ BusinessLayer/Models/UserDTOToGet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Models
+{
+    public class UserDTOToGet
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get;set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        List<PlaylistDTOToGive> Playlists { get; set; }
+    }
+}
