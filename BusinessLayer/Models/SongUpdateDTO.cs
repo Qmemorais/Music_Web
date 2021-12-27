cb89922949fcfae8c754212cd8920f70f9db6660
Qmemorais <qmemorais@gmail.com>
2021-12-27 16:13:43 +0200

    Create SongUpdateDTO.cs
    create model to update fields in Song model from DB

---
+++ BusinessLayer/Models/SongUpdateDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BusinessLayer.Models
+{
+    public class SongUpdateDTO
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
