af524a445b587604cf7b5ce851053b6b43d2a84f
Qmemorais <qmemorais@gmail.com>
2021-12-27 17:02:01 +0200

    Create IUserService.cs
    create methods interfaces for User

---
+++ BusinessLayer/Services/Interfaces/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLayer.Models;
+
+namespace BusinessLayer.Services.Interfaces
+{
+    public interface IUserService
+    {
+        public void CreateUser(UserCreateDTO userCreate);
+        public void UpdateUser(UserUpdateDTO userUpdate);
+        public void DeleteUser(Guid id);
+        public List<UserDTOToGet> GetUsers();
+        public UserDTOToGet GetUserById(Guid id);
+        public void AddPlaylistToUser(Guid userId, Guid playlistId);
+        public List<UserDTOToGet> GetUsersByPlaylist(Guid playlistId);
+        public List<UserDTOToGet> GetUsersByCountry(string countryName);
+        public List<UserDTOToGet> GetUsersByAge(int age);
+
+    }
+}
