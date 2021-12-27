0d6383e7e9d83896a86e25721a111cd8c0df5299
Qmemorais <qmemorais@gmail.com>
2021-12-27 17:23:23 +0200

    Create IAlbumService.cs
    create methods interfaces for Album

---
+++ BusinessLayer/Services/Interfaces/IAlbumService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLayer.Models;
+
+namespace BusinessLayer.Services.Interfaces
+{
+    public interface IAlbumService
+    {
+        public void CreateAlbum(AlbumCreateDTO albumCreate);
+        public void UpdateAlbum(AlbumUpdateDTO albumUpdate,Guid albumId);
+        public void DeleteAlbum(Guid id);
+        public List<AlbumDTOToGet> GetAlbums();
+        public AlbumDTOToGet GetAlbumById(Guid id);
+        public List<AlbumDTOToGet> GetAllAlbumsByArtist(Guid artistId);
+        public List<AlbumDTOToGet> GetAllAlbumsByTime(DateTime time);
+    }
+}
