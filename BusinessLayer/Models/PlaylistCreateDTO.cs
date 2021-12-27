d4f7be3ae28fa35cc7a560a35ad2c6156f68eb66
Qmemorais <qmemorais@gmail.com>
2021-12-27 16:03:24 +0200

    Create PlaylistCreateDTO.cs
    create model to create Playlist in DB

---
+++ BusinessLayer/Models/PlaylistCreateDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BusinessLayer.Models
+{
+    public class PlaylistCreateDTO
+    {
+        public string Name { get; set; }
+        public Guid UserOwnerId { get; set; }
+    }
+}
