519067ab2a6b4d85aac67c4ba6ca246ba3677df2
Qmemorais <qmemorais@gmail.com>
2021-12-27 15:57:04 +0200

    Create PlaylistDTO.cs
    create model of Playlist to give them from User

---
+++ BusinessLayer/Models/PlaylistDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusinessLayer.Models
+{
+    public class PlaylistDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid UserOwnerId { get; set; }
+        public DateTime TimeToPlay { get; set; }
+    }
+}
