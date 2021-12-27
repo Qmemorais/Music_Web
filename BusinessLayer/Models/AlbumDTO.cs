333d040cc8681fc4687d2c2319675c62fa03207a
Qmemorais <qmemorais@gmail.com>
2021-12-27 16:26:03 +0200

    Create AlbumDTO.cs
    create model of album to take from Artist model

---
+++ BusinessLayer/Models/AlbumDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusinessLayer.Models
+{
+    public class AlbumDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public Guid AtristId { get; set; }
+    }
+}
