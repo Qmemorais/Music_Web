be344b8e09b8dda65ca7a7119cf55946d7a2ca81
Qmemorais <qmemorais@gmail.com>
2021-12-27 16:18:59 +0200

    update fields from Song models
    update field Author to ArtistID

--- BusinessLayer/Models/SongDTO.cs
+++ BusinessLayer/Models/SongDTO.cs
@@ -8,7 +8,7 @@ namespace BusinessLayer.Models
         public string Title { get; set; }
         public string Genre { get; set; }
         public DateTime Time { get; set; }
-        public Guid AuthorId { get; set; }
+        public Guid ArtistId { get; set; }
         public Guid AlbumId { get; set; }
     }
 }
--- BusinessLayer/Models/SongDTOToGet.cs
+++ BusinessLayer/Models/SongDTOToGet.cs
@@ -9,7 +9,7 @@ namespace BusinessLayer.Models
         public string Title { get; set; }
         public string Genre { get; set; }
         public DateTime Time { get; set; }
-        public Guid AuthorId { get; set; }
+        public Guid ArtistId { get; set; }
         public Guid AlbumId { get; set; }
         public List<PlaylistDTO> Playlists { get; set; }
     }
