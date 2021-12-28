9008707c842686fb6fadd4a01aa06ed92ba9b624
Qmemorais <qmemorais@gmail.com>
2021-12-28 17:33:39 +0200

    Update UnitOfWork.cs
    remove unusable libraries

--- DataL/Repositories/UnitOfWork.cs
+++ DataL/Repositories/UnitOfWork.cs
@@ -1,11 +1,6 @@
 ﻿using DataLayer.Context;
 using DataLayer.Interfaces;
 using DataLayer.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataLayer.Repositories
 {
