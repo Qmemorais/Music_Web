b695dc25deb65f01d3627f4fe5d1b38e74d7b392
Qmemorais <qmemorais@gmail.com>
2022-01-11 15:28:03 +0200

    Update ArtistService.cs
    create methods CRUD for Artist model

--- BusinessLayer/Services/ArtistService.cs
+++ BusinessLayer/Services/ArtistService.cs
@@ -22,12 +22,25 @@ namespace BusinessLayer.Services
 
         public void CreateArtist(ArtistCreateDTO artistCreate)
         {
-            throw new NotImplementedException();
+            var mappedArtist = _mapper.Map<Artist>(artistCreate);
+            var anyArtistWithName = _uow.Artists.Find(a => a.Name == artistCreate.Name).First();
+
+            if(anyArtistWithName == null)
+            {
+                _uow.Artists.Create(mappedArtist);
+                _uow.Save();
+            }
         }
 
         public void DeleteArtist(Guid id)
         {
-            throw new NotImplementedException();
+            var artist = _uow.Artists.Get(id);
+
+            if (artist != null)
+            {
+                _uow.Artists.Delete(id);
+                _uow.Save();
+            }
         }
 
         public List<ArtistDTO> GetAllArtistsByCountry(string country)
@@ -47,7 +60,21 @@ namespace BusinessLayer.Services
 
         public void UpdateArtist(ArtistUpdateDTO artistUpdate, Guid artistId)
         {
-            throw new NotImplementedException();
+            var artist = _uow.Artists.Get(artistId);
+
+            if (artist != null)
+            {
+                artist.Country = artistUpdate.Country;
+                if (artist.Name != artistUpdate.Name)
+                {
+                    var anyArtistWithNewName = _uow.Artists.Find(a => a.Name == artistUpdate.Name).First();
+
+                    if (anyArtistWithNewName == null)
+                        artist.Name = artistUpdate.Name;
+                }
+                _uow.Artists.Update(artist);
+                _uow.Save();
+            }
         }
     }
 }
