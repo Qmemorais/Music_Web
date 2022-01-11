c7b77ccb74773ca4a87a9e49989f1d6be7eafa6d
Qmemorais <qmemorais@gmail.com>
2022-01-11 15:37:44 +0200

    Create AlbumService.cs
    Create parametres and constructor AlbumService

---
+++ BusinessLayer/Services/AlbumService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using BusinessLayer.Models;
+using BusinessLayer.Services.Interfaces;
+using DataLayer.Interfaces;
+using DataLayer.Models;
+
+namespace BusinessLayer.Services
+{
+    public class AlbumService : IAlbumService
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _uow;
+
+        public AlbumService(IUnitOfWork uow, IMapper mapper)
+        {
+            _mapper = mapper;
+            _uow = uow;
+        }
+    }
+}
