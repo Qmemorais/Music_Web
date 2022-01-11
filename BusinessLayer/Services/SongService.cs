c52a7f37aaf3748b281d54fab24b15ca6c1894f9
Qmemorais <qmemorais@gmail.com>
2022-01-11 14:00:10 +0200

    Create SongService.cs
    Create parametres and constructor SongService

---
+++ BusinessLayer/Services/SongService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using BusinessLayer.Models;
+using BusinessLayer.Services.Interfaces;
+using DataLayer.Interfaces;
+using DataLayer.Models;
+
+namespace BusinessLayer.Services
+{
+    public class SongService:ISongService
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _uow;
+
+        public SongService(IUnitOfWork uow, IMapper mapper)
+        {
+            _mapper = mapper;
+            _uow = uow;
+        }
+    }
+}
