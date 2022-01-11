bcb4d408d0bdcaaa256512e1186eb4d964717a8e
Qmemorais <qmemorais@gmail.com>
2022-01-11 13:23:36 +0200

    Create PlaylistService.cs
    Create parametres and constructor PlaylistService

---
+++ BusinessLayer/Services/PlaylistService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using BusinessLayer.Models;
+using BusinessLayer.Services.Interfaces;
+using DataLayer.Interfaces;
+using DataLayer.Models;
+
+namespace BusinessLayer.Services
+{
+    public class PlaylistService:IPlaylistService
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _uow;
+
+        public PlaylistService(IUnitOfWork uow, IMapper mapper)
+        {
+            _mapper = mapper;
+            _uow = uow;
+        }
+    }
+}
